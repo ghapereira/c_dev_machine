37c9d247c4476eb9a36b0c0e450143cb99d24a97
Gustavo Pereira <ghapereira@gmail.com>
2016-03-05 17:53:41 -0300

    First commit

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+# Install vim
+#------------
+apt-get install -y vim
+
+# Install cURL
+# ------------
+apt-get install -y curl
+
+# Install Git
+# -----------
+apt-get install -y git-core
+
+# Install Ag
+# ----------
+apt-get install -y silversearcher-ag
+
+# Install Valgrind
+# ---------
+apt-get install -y valgrind
